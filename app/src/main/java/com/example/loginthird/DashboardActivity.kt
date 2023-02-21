e55c33f7bca66d752cc67b48e9b71187a11960f3
muhammad <mhdsemps@gmail.com>
2023-02-21 12:06:06 +0300

    toolbars reinstated

--- app/src/main/java/com/example/loginthird/DashboardActivity.kt
+++ app/src/main/java/com/example/loginthird/DashboardActivity.kt
@@ -14,6 +14,8 @@ class DashboardActivity : AppCompatActivity() {
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initToolbar()
+
         binding.buttonCreateAppointment.setOnClickListener {
             openActivity(CreateAppointmentActivity::class.java)
         }
@@ -21,5 +23,13 @@ class DashboardActivity : AppCompatActivity() {
         binding.buttonOpenAppointmentList.setOnClickListener {
             openActivity(AppointmentListActivity::class.java)
         }
+    }
+
+    private fun initToolbar() {
+        val toolbar = binding.toolbar.toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = "Dashboard"
+//        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        Tools.setSystemBarColor(this)
     }
 }
